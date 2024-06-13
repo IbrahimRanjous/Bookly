8d0b226354d5bda424f1a3489d77809177cb620b
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-06-13 17:16:21 +0300

    adding sliding animation to splash view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,40 @@
+import 'package:bookly/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SpalshViewBody extends StatelessWidget {
+class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
+
+  @override
+  State<SpalshViewBody> createState() => _SpalshViewBodyState();
+}
+
+class _SpalshViewBodyState extends State<SpalshViewBody>
+    with SingleTickerProviderStateMixin {
+  //  this variable gives me values from 0 => 1
+  late AnimationController animationController;
+  //  this variable we created to controle on the values that animationController give us to another values
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+
+    slidingAnimation.addListener(() {});
+  }
+
+  // the controller should be disposed to not causing memory leak
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +46,8 @@ class SpalshViewBody extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        //  to prevent rebuilding the whole screen so we just rebuild the animated things
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
