183168f9e53cbfc246ba04903bfc2b2abeb61d29
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-06-14 14:28:01 +0300

    change the animation text from controller to package called flutter_animate

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,40 +1,11 @@
-import 'package:bookly/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
-class SpalshViewBody extends StatefulWidget {
+class SpalshViewBody extends StatelessWidget {
   const SpalshViewBody({super.key});
-
-  @override
-  State<SpalshViewBody> createState() => _SpalshViewBodyState();
-}
-
-class _SpalshViewBodyState extends State<SpalshViewBody>
-    with SingleTickerProviderStateMixin {
-  //  this variable gives me values from 0 => 1
-  late AnimationController animationController;
-  //  this variable we created to controle on the values that animationController give us to another values
-  late Animation<Offset> slidingAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-        .animate(animationController);
-
-    slidingAnimation.addListener(() {});
-  }
-
-  // the controller should be disposed to not causing memory leak
-  @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,8 +17,10 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
         const SizedBox(
           height: 4,
         ),
-        //  to prevent rebuilding the whole screen so we just rebuild the animated things
-        SlidingText(slidingAnimation: slidingAnimation),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+        ).animate().slide(begin: const Offset(0, 4), end: const Offset(0, 0)),
       ],
     );
   }
